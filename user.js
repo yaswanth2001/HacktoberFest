0631eed253cc4baaf1e8746dd1b748d64bf72bc2
coder nibba <61580328+codernibba@users.noreply.github.com>
2020-10-01 13:32:38 +0530

    Update user.js

--- user.js
+++ user.js
@@ -8,7 +8,7 @@ let cards = [
     ["Your Name", "./img/username.png", "Your Bio", "https://github.com/IIITians-Network"],
     ["Your Name", "./img/username.png", "Your Bio", "https://github.com/IIITians-Network"],
     ["Your Name", "./img/username.png", "Your Bio", "https://github.com/IIITians-Network"],
-    ["Your Name", "./img/username.png", "Your Bio", "https://github.com/IIITians-Network"],
+    ["Codernibba", "./img/codernibba.gif", "In relationship with @codernibbi", "https://github.com/codernibba"],
     ["Vimal Kumar", "./img/vimalverma558.jpg", "Web Developer and designer", "https://github.com/vimalverma558"]
     ["Vikas shah", "./img/vik.jpg", "Web Developer", "https://github.com/vikasshah0"]
 
